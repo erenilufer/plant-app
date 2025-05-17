cb078f062c6166022d2177ac7555f733c83a66c4
erenilufer <eren.nilufer@outlook.com>
2025-05-17 18:14:34 +0300

    feat(core): added Rubik font

--- App.tsx
+++ App.tsx
@@ -1,8 +1,30 @@
 import RootNavigator from "./src/navigators/RootNavigator";
 import { Provider } from "react-redux";
 import { store } from "./src/store/store";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    regular: require("./assets/fonts/Rubik-Regular.ttf"),
+    bold: require("./assets/fonts/Rubik-Bold.ttf"),
+    medium: require("./assets/fonts/Rubik-Medium.ttf"),
+    light: require("./assets/fonts/Rubik-Light.ttf"),
+    semiBold: require("./assets/fonts/Rubik-SemiBold.ttf"),
+    extraBold: require("./assets/fonts/Rubik-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
       <RootNavigator />
