48c1845a74c822314a1d30ffa6045a35af255a13
erenilufer <eren.nilufer@outlook.com>
2025-05-18 05:40:49 +0300

    feat(core): added app colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,41 @@
+export const colors = {
+  primary: "#28AF6E",
+  primaryLight: "#5DB075",
+  primaryDark: "#13231B",
+
+  textPrimary: "#13231B",
+  textSecondary: "#A8A8A8",
+  textWhite: "#FFFFFF",
+  textGold: "#E4B046",
+
+  bgPrimary: "#FBFAFA",
+  bgWhite: "#FFFFFF",
+  bgDark: "#101E17",
+  bgBlack: "#000000",
+  bgPremium: "#24201A",
+
+  borderLight: "#EBEBEC",
+  borderTranslucent: "#FFFFFF4D",
+  borderSeparator: "#3C3C4340",
+
+  cardLight: "#F4F6F6",
+  cardDark: "#FFFFFF14",
+
+  iconActive: "#5DB075",
+  iconInactive: "#BDBDBD",
+
+  tabBarBg: "#FFFFFFEB",
+
+  dotActive: "#13231B",
+  dotInactive: "#E4E4E4",
+
+  overlayDark: "rgba(0, 0, 0, 0.3)",
+  overlayLight: "rgba(255, 255, 255, 0.2)",
+
+  textTranslucent: "#FFFFFF80",
+
+  success: "#28AF6E",
+  warning: "#E4B046",
+};
+
+export default colors;
